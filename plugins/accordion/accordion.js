206bb1bb2c5723b3751e18e34c5220f99c6f25d2
Tomer Lichtash <tomerl@wix.com>
2012-12-09 19:01:39 +0200

    added sprites

--- plugins/accordion/accordion.js
+++ plugins/accordion/accordion.js
@@ -45,8 +45,8 @@
 			var $this = $(this);
 			if ($this.find('.' + opt.contentClass).is(':hidden')) {
 				$('.' + opt.triggerClass).removeClass(opt.activeClass).find('.' + opt.contentClass).slideUp(opt.animationTime, opt.ease);
-				$this.toggleClass(opt.activeClass).find('.'+opt.contentClass).slideDown(opt.animationTime, opt.ease);
-			}
+				$this.toggleClass(opt.activeClass).find('.'+opt.contentClass).fadeIn('fast').slideDown(opt.animationTime, opt.ease);
+            }
 			return false;
 		});
 	};

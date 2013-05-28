0a519d165d5380d3547fb313df5a7d01696b6d70
Barak <baraki@wix.com>
2013-05-28 16:05:45 +0300

    refactor for glued comp

--- javascripts/views/settings.js
+++ javascripts/views/settings.js
@@ -1,18 +1,4 @@
 $(document).ready(function() {
-    $('.accordion').Accordion();
-    $('.color-selector').ColorPicker();
-    $('.slider').Slider({ type: "Value" });
-    $('.checkbox').Checkbox({ checked: true });
-    $('.radiobuttons').Radio({ checked: 0 });
-    $('.layouts').Radio({el: "figure figcaption", checked: 1});
-    $('.example1-color-picker').ColorPicker();
-    $('.example1 .radiobuttons').Radio({ checked: 0 });
-//	$('.glued-positioning').GluedPosition({initWithBinding: true});
-//	$('.glued-positioning').GluedPosition({initWithBinding: true, placements: ['TOP_LEFT', 'TOP', 'BOTTOM']});
-//	$('.glued-positioning').GluedPosition({
-//        dropDownChange: console.log.bind(console, 'drop'),
-//        dropDownCreate: console.log.bind(console, 'dropc'),
-//        sliderChange: console.log.bind(console, 'slide'),
-//        sliderCreate: console.log.bind(console, 'slidec')
-//    });
+	wixUiAutoStart();
+	//Init Other Plugins Here...	
 });

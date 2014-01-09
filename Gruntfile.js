dd661630b430468368d9486b8e39ad70a4b5bd08
Barak <baraki@wix.com>
2014-01-09 13:02:31 +0200

    after merge

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,8 +140,8 @@ module.exports = function (grunt) {
 		karma: {
 			unit: {
 				configFile: 'karma.conf.js',
-				singleRun: true,
-				browsers: ['PhantomJS']
+				singleRun: true
+			//	browsers: ['Chrome']
 			}
 		},
 

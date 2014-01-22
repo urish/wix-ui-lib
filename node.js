009dff9f0dad11bb8e4872d787116d5ea9731bed
Lior Shefer <lior.shefer@wix.com>
2014-01-22 10:05:35 +0200

    Make node.js simpler

--- node.js
+++ node.js
@@ -1,20 +1,11 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.redirect("/build/settings.html");
+    res.redirect("build/settings.html");
 });
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/build')));
-app.use(express.methodOverride());
-app.use(express.bodyParser());
-app.use(express.errorHandler({
-    dumpExceptions: true,
-    showStack: true
-}));
-
+app.use("/build", express.static(__dirname + '/build'));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {

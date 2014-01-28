7f180dfa675263fc0e2ded16e44e4b92875de80f
Lior Shefer <lior.shefer@wix.com>
2014-01-28 23:35:44 +0200

    Css section in docs

--- docs/ScrollInterations.js
+++ docs/ScrollInterations.js
@@ -6,10 +6,11 @@ DocsApp.Classes.ScrollInterations = function () {
 	var $win = $(window);
 	var $doc = $(document);	
 	var $body = $('body');
-	
+	var $components = $("#components");
+	var $css = $("#css");
 	return {
 		mixin: function(obj){
-			if(obj.bindScrollAnimation || obj.bindScrollInteraction){
+			if(obj.bindScrollAnimation || obj.bindScrollInteraction || obj.bindHeaderScroll){
 				throw new Error('Object is already implements ScrollInterations');
 			}
 			obj.bindScrollAnimation = this.bindScrollAnimation;
@@ -27,24 +28,18 @@ DocsApp.Classes.ScrollInterations = function () {
 				});
 			});
 		},
+
 		bindHeaderScroll : function(){
 			var navPos = $('.page-welcome').height() - 50;
 			$win.resize(function() {
-				$('.navigation').toggleClass('shown', $(document).scrollTop() >= navPos);
+				$('.navigation').toggleClass('shown', $doc.scrollTop() >= navPos);
 			});
 			$doc.bind('scroll touchmove', function() {
-				$('.navigation').toggleClass('shown', $(document).scrollTop() >= navPos);
-				var offset  = $('#components').offset().top - $(window).scrollTop();
-				if ( offset < 40 ) {
-					$(".cmp-sidebar").parent().addClass("scroll");
-				}
-				if (offset > 40) {
-					$(".cmp-sidebar").parent().removeClass("scroll");
-				}
+				$('.navigation').toggleClass('shown', $doc.scrollTop() >= navPos);
 			});
 		},
 
-		bindScrollInteraction : function startScrollInteraction() {
+		bindScrollInteraction : function() {
 			var $inter = $(dataInter);
 			var allHashes = $(dataTarget).toArray();
 			var tollerace = 30;
@@ -56,24 +51,25 @@ DocsApp.Classes.ScrollInterations = function () {
 			var dir = 0;
 			var UP = 1
 			var DOWN = -1
+			var direction = winScrollTop >= _winScrollTop ? UP : DOWN;;
 			var update = function () {
 				winHeight = $win.height();
 				docHeight = $doc.height();
 				_winScrollTop = $win.scrollTop();
-				dir = winScrollTop >= _winScrollTop ? UP : DOWN;
 				winScrollTop = _winScrollTop;
 			};
 			var findElementOverTheScroll = function () {
-				return allHashes.filter(function (el, i) {
+				var elements = allHashes.filter(function (el, i) {
 					if (winScrollTop + winHeight > docHeight - $(allHashes[allHashes.length - 1]).height() / 8) {
 						return true;
 					}
-					if (dir === UP) {
+					if (direction === UP) {
 						return winScrollTop > ($(el).position().top - ($(allHashes[i - 1]).height() / 2 || 0));
 					} else {
 						return winScrollTop + tollerace > $(el).position().top;
 					}
 				});
+				return elements.pop();
 			};
 			var updateCss = function (el) {
 				$inter.removeClass(currentViewed);
@@ -88,10 +84,62 @@ DocsApp.Classes.ScrollInterations = function () {
 			$win.scroll(function (evt) {
 				clearTimeout(timeout);
 				timeout = setTimeout(function () {
+						if(window.location.hash==="#css"){return ;}
 						update();
-						var over = findElementOverTheScroll();
-						updateCss(over.pop());
+						var element = findElementOverTheScroll();
+						updateCss(element);
 					}, 20);
+			});
+
+			$doc.bind('scroll touchmove', function() {
+				var componentsOffset  = $components.offset().top - $(window).scrollTop();
+				var cssOffset  = $css.offset().top - $(window).scrollTop();
+
+				var $componentsSidebar = $components.find(".js-cmp-sidebar-wrapper");
+				var $cssSidebar = $css.find(".js-cmp-sidebar-wrapper");
+
+				var offset = 40;
+
+				if (componentsOffset < offset ) {
+					var element = findElementOverTheScroll();
+					if ($(element).attr('id') === 'Tooltip-entry') {
+						if($componentsSidebar.hasClass("scroll")){
+							var t = $componentsSidebar.offset().top;
+							$componentsSidebar.animate({
+								top: t
+							}, 20, function() {
+								$componentsSidebar.css('position', 'absolute');
+								$componentsSidebar.removeClass("scroll");
+								$cssSidebar.removeClass("scroll").fadeIn(200);
+							});
+						} else if(direction == UP) {
+							$componentsSidebar.addClass("scroll");
+							$componentsSidebar.removeAttr('style');
+						}
+					} else {
+						$componentsSidebar.addClass("scroll");
+						$componentsSidebar.removeAttr('style');
+						$cssSidebar.removeClass("scroll")
+
+						if (($(element).attr('id') === 'Buttons-entry' || $(element).attr('id') === 'Icons-entry')) {
+							$cssSidebar.show();
+						} else if(element) {
+							$cssSidebar.hide();
+						}
+					}
+				}
+				if (componentsOffset > offset) {
+					$componentsSidebar.removeClass("scroll");
+				}
+
+
+				if (cssOffset < offset) {
+					//$cssSidebar.hide();
+					$componentsSidebar.removeClass("scroll");
+					$cssSidebar.addClass("scroll").fadeIn(200);
+				} else {
+					$cssSidebar.removeClass("scroll");
+				}
 			});
 		}
 	};

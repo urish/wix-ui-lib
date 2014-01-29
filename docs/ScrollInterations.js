a28473a31910e34827370b56eab0343237408c30
Lior Shefer <lior.shefer@wix.com>
2014-01-29 10:20:02 +0200

    Using a sticky plugin to cleanup the code

--- docs/ScrollInterations.js
+++ docs/ScrollInterations.js
@@ -6,8 +6,8 @@ DocsApp.Classes.ScrollInterations = function () {
 	var $win = $(window);
 	var $doc = $(document);	
 	var $body = $('body');
-	var $components = $("#components");
-	var $css = $("#css");
+	var noScroll = false;
+
 	return {
 		mixin: function(obj){
 			if(obj.bindScrollAnimation || obj.bindScrollInteraction || obj.bindHeaderScroll){
@@ -21,12 +21,24 @@ DocsApp.Classes.ScrollInterations = function () {
 			$body.on('click', dataInter, function (evt) {
 				evt.preventDefault();
 				var id = evt.target.getAttribute('href');
+				if (evt.target.hasAttribute('data-no-scroll')){
+					noScroll = true;
+					$('[href]').removeClass(currentViewed);
+				} else {
+					noScroll = false;
+				}
 				$body.animate({
 					scrollTop : $(id).offset().top
 				}, 160, function () {
 					window.location.hash = id;
 				});
 			});
+
+			setTimeout(function(){
+				$(".js-sidebar").stick_in_parent({offset_top: 80});
+			}, 100);
+
+
 		},
 
 		bindHeaderScroll : function(){
@@ -51,11 +63,11 @@ DocsApp.Classes.ScrollInterations = function () {
 			var dir = 0;
 			var UP = 1
 			var DOWN = -1
-			var direction = winScrollTop >= _winScrollTop ? UP : DOWN;;
 			var update = function () {
 				winHeight = $win.height();
 				docHeight = $doc.height();
 				_winScrollTop = $win.scrollTop();
+				dir = winScrollTop >= _winScrollTop ? UP : DOWN;
 				winScrollTop = _winScrollTop;
 			};
 			var findElementOverTheScroll = function () {
@@ -63,7 +75,7 @@ DocsApp.Classes.ScrollInterations = function () {
 					if (winScrollTop + winHeight > docHeight - $(allHashes[allHashes.length - 1]).height() / 8) {
 						return true;
 					}
-					if (direction === UP) {
+					if (dir === UP) {
 						return winScrollTop > ($(el).position().top - ($(allHashes[i - 1]).height() / 2 || 0));
 					} else {
 						return winScrollTop + tollerace > $(el).position().top;
@@ -74,72 +86,18 @@ DocsApp.Classes.ScrollInterations = function () {
 			var updateCss = function (el) {
 				$inter.removeClass(currentViewed);
 				if (el) {
-					window.location.hash = el.id;
+					//window.location.hash = el.id;
 					$('[href="#' + el.id + '"]').addClass(currentViewed);
-				} else {
-					//$(".cmp-sidebar").removeClass("scroll");
-					//window.location.hash = 'demo';
 				}
 			}
 			$win.scroll(function (evt) {
 				clearTimeout(timeout);
 				timeout = setTimeout(function () {
-						if(window.location.hash==="#css"){return ;}
+						if(noScroll) return;
 						update();
 						var element = findElementOverTheScroll();
 						updateCss(element);
 					}, 20);
-			});
-
-			$doc.bind('scroll touchmove', function() {
-				var componentsOffset  = $components.offset().top - $(window).scrollTop();
-				var cssOffset  = $css.offset().top - $(window).scrollTop();
-
-				var $componentsSidebar = $components.find(".js-cmp-sidebar-wrapper");
-				var $cssSidebar = $css.find(".js-cmp-sidebar-wrapper");
-
-				var offset = 40;
-
-				if (componentsOffset < offset ) {
-					var element = findElementOverTheScroll();
-					if ($(element).attr('id') === 'Tooltip-entry') {
-						if($componentsSidebar.hasClass("scroll")){
-							var t = $componentsSidebar.offset().top;
-							$componentsSidebar.animate({
-								top: t
-							}, 20, function() {
-								$componentsSidebar.css('position', 'absolute');
-								$componentsSidebar.removeClass("scroll");
-								$cssSidebar.removeClass("scroll").fadeIn(200);
-							});
-						} else if(direction == UP) {
-							$componentsSidebar.addClass("scroll");
-							$componentsSidebar.removeAttr('style');
-						}
-					} else {
-						$componentsSidebar.addClass("scroll");
-						$componentsSidebar.removeAttr('style');
-						$cssSidebar.removeClass("scroll")
-
-						if (($(element).attr('id') === 'Buttons-entry' || $(element).attr('id') === 'Icons-entry')) {
-							$cssSidebar.show();
-						} else if(element) {
-							$cssSidebar.hide();
-						}
-					}
-				}
-				if (componentsOffset > offset) {
-					$componentsSidebar.removeClass("scroll");
-				}
-
-
-				if (cssOffset < offset) {
-					//$cssSidebar.hide();
-					$componentsSidebar.removeClass("scroll");
-					$cssSidebar.addClass("scroll").fadeIn(200);
-				} else {
-					$cssSidebar.removeClass("scroll");
-				}
 			});
 		}
 	};
